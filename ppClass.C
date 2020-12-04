36bc53b90d2b8e66860dbe2af3f107f491e41b88
Gijs van Weelden <gijsvanweelden@Gijs-MBP.local>
2020-12-04 13:46:45 +0100

    Fixed histogram saving, added canvases

--- ppClass.C
+++ ppClass.C
@@ -1,5 +1,6 @@
 #define ppClass_cxx
 #include "ppClass.h"
+#include <TH1.h>
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
@@ -32,21 +33,54 @@ void ppClass::Loop()
   if (fChain == 0) return;
 
   Long64_t nentries = fChain->GetEntriesFast();
+  Long64_t nbytes = 0, nb = 0;
   float_t ptL = 0;
   // ptCuts=1: high pt, ptL>120 GeV, ptS>30 GeV
   // ptCuts=0: low pt, ptL>35 GeV, ptS>10 GeV
   bool ptCuts = 0;
+  /*
+     string htitle = "Acoplanarity dphi";
+     string hwtitle = "Acoplanarity dphi (weighted):";
 
+     if(ptCuts){
+     htitle += "ptL>120 GeV, ptS>30 GeV";
+     hwtitle += "ptL>120 GeV, ptS>30 GeV";
+     }
+     else{
+     htitle += "ptL>35 GeV, ptS>10 GeV";
+     hwtitle += "ptL>35 GeV, ptS>10 GeV"; 
+     }   
+
+     htitle += ", |eta_jet|<2";
+     hwtitle += ", |eta_jet|<2";
+     string title1 = &htitle;
+     string title2 = &hwtitle;
+   */
+  TH1F *hdphi = new TH1F("Hist_dphi","a",100,0,3.2);
+  TH1F *hwdphi = new TH1F("Hist_w_dphi","b",100,0,3.2);
   if(ptCuts){
-    TH1F *hdphi = new TH1F("Hist_dphi_highpt","Acoplanarity dphi: ptL> 120 GeV, ptS> 30 GeV, |eta_jet|<2",100,0,3.2);
-    TH1F *hdphiw = new TH1F("Hist_dphi_highpt","Acoplanarity dphi (weighted): ptL> 120 GeV, ptS> 30 GeV, |eta_jet|<2",100,0,3.2);
+    hdphi->SetTitle("Acoplanarity dphi: ptL> 120 GeV, ptS> 30 GeV, |eta_jet|<2");
+    hwdphi->SetTitle("Acoplanarity dphi (weighted): ptL> 120 GeV, ptS> 30 GeV, |eta_jet|<2");
   }
   else{
-    TH1F *hdphi = new TH1F("Hist_dphi_lowpt","Acoplanarity dphi: ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2",100,0,3.2);
-    TH1F *hdphiw = new TH1F("Hist_dphi_lowpt","Acoplanarity dphi (weighted): ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2",100,0,3.2);
+    hdphi->SetTitle("Acoplanarity dphi: ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2");
+    hwdphi->SetTitle("Acoplanarity dphi (weighted): ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2");
   }
+  //TH1F *hdphi = new TH1F("Hist_dphi_lowpt","Acoplanarity dphi: ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2",100,0,3.2);
+  //TH1F *hwdphi = new TH1F("Hist_dphi_lowpt","Acoplanarity dphi (weighted): ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2",100,0,3.2);
 
-  Long64_t nbytes = 0, nb = 0;
+
+  /*
+     if(ptCuts){
+     TH1F *hdphi = new TH1F("Hist_dphi_highpt","Acoplanarity dphi: ptL> 120 GeV, ptS> 30 GeV, |eta_jet|<2",100,0,3.2);
+     TH1F *hwdphi = new TH1F("Hist_dphi_highpt","Acoplanarity dphi (weighted): ptL> 120 GeV, ptS> 30 GeV, |eta_jet|<2",100,0,3.2);
+     }
+     else{
+     TH1F *hdphi = new TH1F("Hist_dphi_lowpt","Acoplanarity dphi: ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2",100,0,3.2);
+     TH1F *hwdphi = new TH1F("Hist_dphi_lowpt","Acoplanarity dphi (weighted): ptL> 35 GeV, ptS> 10 GeV, |eta_jet|<2",100,0,3.2);
+     }
+   */
+
   for (Long64_t jentry=0; jentry<nentries;jentry++) {
     Long64_t ientry = LoadTree(jentry);
     if (ientry < 0) break;
@@ -71,38 +105,57 @@ void ppClass::Loop()
        else if(ptL>120 && pt>30){
     //cout << "Acoplanarity: " << abs(dphi) << "\n";
     hdphi->Fill(abs(dphi));
-    //hdphiw->Fill(abs(dphi),evwt);
+    //hwdphi->Fill(abs(dphi),evwt);
     }
     else if(ptL>35 && pt>10){
     //hdphi->Fill(abs(dphi));
-    hdphiw->Fill(abs(dphi),evwt);
+    hwdphi->Fill(abs(dphi),evwt);
     }
 
      */
     else{
       if(ptCuts && ptL>120 && pt>30){
         hdphi->Fill(abs(dphi));
-        hdphiw->Fill(abs(dphi),evwt);
+        hwdphi->Fill(abs(dphi),evwt);
       }
       else if(ptL>35 && pt>10){
         hdphi->Fill(abs(dphi));
-        hdphiw->Fill(abs(dphi),evwt);
+        hwdphi->Fill(abs(dphi),evwt);
       }
     }
   }
+  // TODO: add canvas
+  TCanvas *c1 = new TCanvas();
   hdphi->Draw();
+  TCanvas *c2 = new TCanvas();
+  hwdphi->Draw();
   // TODO: write the histogram to its own .root file
-  string houtname = "pp2tev76_hdphi";
-  string hwoutname = houtname + "w";
+  /*
+     string houtname = "pp2tev76_hdphi";
+     string hwoutname = houtname + "w";
+
+     if(ptCuts){
+     houtname += "high_pt";
+     hwoutname += "high_pt";
+     }
+     else{
+     houtname += "low_pt";
+     hwoutname+= "low_pt";
+     }
+     houtname += ".root";
+     hwoutname += ".root";
+   */
 
   if(ptCuts){
-    houtname += "high_pt";
-    hwoutname += "high_pt";
-    else{
-      houtname += "low_pt";
-      hwoutname+= "low_pt";
-      outputname += ".root";
-
-      hdphi->Write(houtname,TObject::kOverwrite);
-      hdphiw->Write(hwoutname,TObject::kOverwrite);
-    }
+    TFile hdphi_save("pp2tev76_hdphi_highpt.root","RECREATE");
+    hdphi->Write();//hdphi_save,TObject::kOverwrite); 
+    TFile hwdphi_save("pp2tev76_hwdphi_highpt.root","RECREATE");
+    hwdphi->Write();//hwdphi,TObject::kOverwrite);
+  }
+  else{
+    TFile hdphi_save("pp2tev76_hdphi_lowpt.root","RECREATE");
+    hdphi->Write();//hdphi_save,TObject::kOverwrite);
+    TFile hwdphi_save("pp2tev76_hwdphi_lowpt.root","RECREATE");
+    hwdphi->Write();//hwdphi_save,TObject::kOverwrite);
+  }
+}

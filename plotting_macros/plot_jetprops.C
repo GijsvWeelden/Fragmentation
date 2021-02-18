c59fc14d0f769af1804e0d48fd7a848c78d6572a
Gijs van Weelden <gijsvanweelden@hotmail.com>
2021-02-18 11:38:38 +0100

    Fixed normalisation bug

--- plotting_macros/plot_jetprops.C
+++ plotting_macros/plot_jetprops.C
@@ -17,25 +17,26 @@
 //
 //-------------------------------------------------------------
 
-
 void plot_histograms(TH1F *h_pp, TH1F *h_AA_nr, string obs, double min_pt, double max_pt){
   double jetR = 0.4;
-  h_pp->Scale(1./h_pp->Integral(), "width");
-  h_AA_nr->Scale(1./h_AA_nr->Integral(), "width");
+  h_pp->Scale(1./h_pp->Integral());
+  h_AA_nr->Scale(1./h_AA_nr->Integral());
 
   // Check normalisation within precision
   if (abs(h_pp->Integral() -  1.0) > 0.01
       ||
       abs(h_AA_nr->Integral() - 1.0) > 0.01){
     cout << "WARNING: NORMALISATION PROBLEM!" << endl
+      << TString::Format("%s for pt %.0f-%.0f GeV/c", obs.c_str(), min_pt, max_pt).Data() << endl
       << "pp = " << h_pp->Integral() << " (" << h_pp->Integral()-1.0  << ")" << endl 
-      << "AA_norecoil = " << h_AA_nr->Integral() << " (" << h_AA_nr->Integral()-1.0 << ")" << endl;
+      << "AA_norecoil = " << h_AA_nr->Integral() << " (" << h_AA_nr->Integral()-1.0 << ")" << endl;:w
+
   }
 
   // Plot settings
   h_AA_nr->SetStats(0);
   h_AA_nr->SetMarkerStyle(kPlus);
-  h_AA_nr->SetTitle(TString::Format("Jet %s, pt #in [%.0f,%.0f), R = %.1f",
+  h_AA_nr->SetTitle(TString::Format("Jet %s, pt #in [%.0f,%.0f) GeV/c, R = %.1f",
         obs.c_str(), min_pt, max_pt, jetR).Data());
   //h_AA_nr->SetAxisRange(0,30,"X");
   // h_AA_nr y axis
@@ -119,46 +120,48 @@ void plot_jetprops(void){
   string inName = "test";
   TFile *inFile = TFile::Open(TString::Format("./%s.root",inName.c_str()).Data());
   if(!inFile){
-    cout << "File " << inFile << " not found. Aborting programme." << endl;
+    cout << "File " << inFile << " not found. Aborting program." << endl;
     return;
   }
 
   std::vector<double> ptBins = {0.,20.,40.,60.,80.,100.,120.,160.,200.};
-  std::vector<string> obs = {"mass"}; //{"nconst","zg","Rg","nSD","mass","mz2","mr","mr2","rz","r2z"};
+  std::vector<string> obs = {"zg"}; //{"nconst","zg","Rg","nSD","mass","mz2","mr","mr2","rz","r2z"};
   //int ipt = 3;
-  int iobs = 0;
+  //int iobs = 0;
 
-  cout << "Plotting " << obs[iobs] << endl;
-  TList *ppList = (TList*)inFile->Get(TString::Format("pp_%s",
-        obs[iobs].c_str()).Data());
-  if (!ppList) cout << TString::Format("WARNING: pp_%s not found!",
-      obs[iobs].c_str()).Data() << endl; 
-  TList *AA_nrList = (TList*)inFile->Get(TString::Format("AA_norecoil_%s",
-        obs[iobs].c_str()).Data());
-  if (!AA_nrList) cout << TString::Format("WARNING: AA_norecoil_%s not found!",
-      obs[iobs].c_str()).Data() << endl; 
-  TList *AA_rList = (TList*)inFile->Get(TString::Format("AA_recoil_%s",
-        obs[iobs].c_str()).Data());
-  if (!AA_rList) cout << TString::Format("WARNING: AA_recoil_%s not found!",
-      obs[iobs].c_str()).Data() << endl;
+  for (int iobs=0; iobs<obs.size(); iobs++){
+    cout << "Plotting " << obs[iobs] << endl;
+    TList *ppList = (TList*)inFile->Get(TString::Format("pp_%s",
+          obs[iobs].c_str()).Data());
+    if (!ppList) cout << TString::Format("WARNING: pp_%s not found!",
+        obs[iobs].c_str()).Data() << endl; 
+    TList *AA_nrList = (TList*)inFile->Get(TString::Format("AA_norecoil_%s",
+          obs[iobs].c_str()).Data());
+    if (!AA_nrList) cout << TString::Format("WARNING: AA_norecoil_%s not found!",
+        obs[iobs].c_str()).Data() << endl; 
+    TList *AA_rList = (TList*)inFile->Get(TString::Format("AA_recoil_%s",
+          obs[iobs].c_str()).Data());
+    if (!AA_rList) cout << TString::Format("WARNING: AA_recoil_%s not found!",
+        obs[iobs].c_str()).Data() << endl;
 
-  for (int ipt=0; ipt<ptBins.size()-1; ipt++){
-    TH1F *hpp = new TH1F();
-    if (ppList){
-      hpp = (TH1F*)ppList->FindObject(TString::Format("pp_hmass_pt%.0f_%.0f",
-            ptBins[ipt], ptBins[ipt+1]).Data());
+    for (int ipt=0; ipt<ptBins.size()-1; ipt++){
+      TH1F *hpp = new TH1F();
+      if (ppList){
+        hpp = (TH1F*)ppList->FindObject(TString::Format("pp_h%s_pt%.0f_%.0f",
+              obs[iobs].c_str(), ptBins[ipt], ptBins[ipt+1]).Data());
+      }
+      TH1F *hAA_nr = new TH1F();
+      if (AA_nrList){
+        hAA_nr = (TH1F*)AA_nrList->FindObject(TString::Format("AA_norecoil_h%s_pt%.0f_%.0f",
+              obs[iobs].c_str(), ptBins[ipt], ptBins[ipt+1]).Data());
+      }
+      TH1F *hAA_recoil = new TH1F();
+      if (AA_rList){
+        hAA_recoil = (TH1F*)AA_rList->FindObject(TString::Format("hAA_recoil_h%s_pt%.0f_%.0f",
+              obs[iobs].c_str(), ptBins[ipt], ptBins[ipt+1]).Data());
+      }
+      plot_histograms(hpp, hAA_nr, obs[iobs], ptBins[ipt], ptBins[ipt+1]);
     }
-    TH1F *hAA_nr = new TH1F();
-    if (AA_nrList){
-      hAA_nr = (TH1F*)AA_nrList->FindObject(TString::Format("AA_norecoil_hmass_pt%.0f_%.0f",
-            ptBins[ipt], ptBins[ipt+1]).Data());
-    }
-    TH1F *hAA_recoil = new TH1F();
-    if (AA_rList){
-      hAA_recoil = (TH1F*)AA_rList->FindObject(TString::Format("hAA_recoil_hmass_pt%.0f_%.0f",
-            ptBins[ipt], ptBins[ipt+1]).Data());
-    }
-    plot_histograms(hpp, hAA_nr, obs[iobs], ptBins[ipt], ptBins[ipt+1]);
   }
 
   time = (clock() - time)/CLOCKS_PER_SEC;

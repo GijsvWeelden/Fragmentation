ffb69e18ce0e5aadbbefcacac1e66387e339cc36
GijsvWeelden <55794847+GijsvWeelden@users.noreply.github.com>
2022-05-04 15:39:14 +0200

    Implemented hadron ratios

--- plotting_macros/plot_hadron_frag.C
+++ plotting_macros/plot_hadron_frag.C
@@ -9,6 +9,8 @@
 #include "TString.h"
 #include "TLegend.h"
 
+#include "./plotUtils.C"
+
 using std::cout;
 using std::endl;
 
@@ -20,11 +22,15 @@ using std::endl;
 //-------------------------------------------------------------
 
 void charged_VS_full(TH2F *hF, TH2F *hC, string obs, double min_pt, double max_pt, string setting, string hadron);
-void baryon_meson(TH2F *hB, string baryon, TH2F* hM, string meson, string obs, double min_pt, double max_pt, string setting);
+// void spectra_ratio(TH2F *hB, string baryon, TH2F* hM, string meson, string obs, std::vector<double> ptBins, string setting);
+void spectra_ratio(TH2F *hN, string sNum, TH2F* hD, string sDen, std::vector<double> ptBins, string setting);
+// TH1F* make_ratio(TH2F* hBaryon, string baryon, TH2F* hMeson, string meson, double min_pt, double max_pt);
+TH1F* make_ratio(TH2F* hNum, string sNum, TH2F* hDen, string sDen, double min_pt, double max_pt);
 void fit_spectra(TH1F* hFull, TH1F* hCharged, string obs, string hadron);
 
 void plot_hadron_frag(void){
   double time = clock();
+  bool doChargedVSFull = true, doRatios = false;
   // gROOT->SetBatch();
   string inName = "partial_2dhists_frag";
   string sNN = "5tev02";
@@ -57,40 +63,44 @@ void plot_hadron_frag(void){
     return;
   }
 
-  /*
-  TH2F *hC, *hF;
-  for (auto obs : observables){
-    std::cout << "Plotting " << obs << std::endl;
-    hC = (TH2F*)chList->FindObject(TString::Format("pp_charged_%s", obs.c_str()).Data());
-    hF = (TH2F*)fList->FindObject(TString::Format("pp_full_%s", obs.c_str()).Data());
-    charged_VS_full(hF, hC, obs, ptBins[0], ptBins.back(), "pp", "");
-    for (int ipt=0; ipt<ptBins.size()-1; ++ipt){
-      charged_VS_full(hF, hC, obs, ptBins[ipt], ptBins[ipt+1], "pp", "");
-    }
-    for (auto hadron : chHadrons){
-      hC = (TH2F*)chList->FindObject(TString::Format("pp_charged_%s_%s", hadron.c_str(), obs.c_str()).Data());
-      hF = (TH2F*)fList->FindObject(TString::Format("pp_full_%s_%s", hadron.c_str(), obs.c_str()).Data());
-      charged_VS_full(hF, hC, obs, ptBins[0], ptBins.back(), "pp", hadron);
-      for (int ipt = 0; ipt < ptBins.size()-1; ++ipt){
-        charged_VS_full(hF, hC, obs, ptBins[ipt], ptBins[ipt+1], "pp", hadron);
+  if (doChargedVSFull){
+    TH2F *hC, *hF;
+    for (auto obs : observables){
+      std::cout << "Plotting " << obs << std::endl;
+      hC = (TH2F*)chList->FindObject(TString::Format("pp_charged_%s", obs.c_str()).Data());
+      hF = (TH2F*)fList->FindObject(TString::Format("pp_full_%s", obs.c_str()).Data());
+      // charged_VS_full(hF, hC, obs, ptBins[0], ptBins.back(), "pp", "");
+      for (int ipt=0; ipt < 2; ++ipt){ //ipt < ptBins.size()-1; ++ipt){
+        charged_VS_full(hF, hC, obs, ptBins[ipt], ptBins[ipt+1], "pp", "");
       }
+      // for (auto hadron : chHadrons){
+      //   hC = (TH2F*)chList->FindObject(TString::Format("pp_charged_%s_%s", hadron.c_str(), obs.c_str()).Data());
+      //   hF = (TH2F*)fList->FindObject(TString::Format("pp_full_%s_%s", hadron.c_str(), obs.c_str()).Data());
+      //   charged_VS_full(hF, hC, obs, ptBins[0], ptBins.back(), "pp", hadron);
+      //   for (int ipt = 0; ipt < ptBins.size()-1; ++ipt){
+      //     charged_VS_full(hF, hC, obs, ptBins[ipt], ptBins[ipt+1], "pp", hadron);
+      //   }
+      // }
     }
   }
-  */
 
-  TH2F *hM, *hB;
-  string obs = "track_pt";
-  // for (auto meson : chMesons){
-  // for (auto baryon : chBaryons){
-  hM = (TH2F*)chList->FindObject(TString::Format("pp_charged_pi_%s", obs.c_str()).Data());
-  hB = (TH2F*)chList->FindObject(TString::Format("pp_charged_p_%s", obs.c_str()).Data());
-  baryon_meson(hB, "p", hM, "pi", obs, ptBins[0], ptBins.back(), "pp");
-  for (int i = 0; i < ptBins.size()-1; i++){
-    baryon_meson(hB, "p", hM, "pi", obs, ptBins[i], ptBins[i+1], "pp");
+  if (doRatios){
+    TH2F *hPi, *hK, *hP, *hPi0, *hK0L, *hK0S, *hK0, *hLambda0;
+    string obs = "track_pt";
+    hPi = (TH2F*)chList->FindObject(TString::Format("pp_charged_pi_%s", obs.c_str()).Data());
+    hK = (TH2F*)chList->FindObject(TString::Format("pp_charged_K_%s", obs.c_str()).Data());
+    hP = (TH2F*)chList->FindObject(TString::Format("pp_charged_p_%s", obs.c_str()).Data());
+    hPi0 = (TH2F*)fList->FindObject(TString::Format("pp_full_pi0_%s", obs.c_str()).Data());
+    hK0L = (TH2F*)fList->FindObject(TString::Format("pp_full_K0L_%s", obs.c_str()).Data());
+    hK0S = (TH2F*)fList->FindObject(TString::Format("pp_full_K0S_%s", obs.c_str()).Data());
+    hK0 = (TH2F*)fList->FindObject(TString::Format("pp_full_K0_%s", obs.c_str()).Data());
+    hLambda0 = (TH2F*)fList->FindObject(TString::Format("pp_full_Lambda0_%s", obs.c_str()).Data());
+
+    spectra_ratio(hP, "p", hPi, "pi", ptBins, "pp");
+    spectra_ratio(hK, "K", hPi, "pi", ptBins, "pp");
+    spectra_ratio(hK0L, "K0L", hPi0, "pi0", ptBins, "pp");
+    // TODO: Add others as needed
   }
-  // }
-  // }
-  // Do same for neutral
 
   time = (clock() - time)/CLOCKS_PER_SEC;
   cout << "Time taken: " << time << " seconds." << endl;
@@ -171,7 +181,7 @@ void charged_VS_full(TH2F *hF, TH2F *hC, string obs, double min_pt, double max_p
 
   TCanvas *c_obs = new TCanvas(TString::Format("c_%s_pt_%.0f_%.0f", obs.c_str(), min_pt, max_pt).Data(),
                                TString::Format("%s_pt_%.0f_%.0f", obs.c_str(), min_pt, max_pt).Data(),
-                               900, 600);
+                               1600, 1000);
   TPad *pad1 = new TPad("pad1","pad1",0,0.4,1,1);
   pad1->SetBottomMargin(0);
   pad1->SetLeftMargin(0.15);
@@ -210,117 +220,107 @@ void charged_VS_full(TH2F *hF, TH2F *hC, string obs, double min_pt, double max_p
   // delete c_obs;
 }
 
-void baryon_meson(TH2F *hB, string baryon, TH2F* hM, string meson, string obs, double min_pt, double max_pt, string setting){
+void spectra_ratio(TH2F* hNum, string sNum, TH2F* hDen, string sDen, std::vector<double> ptBins, string setting){
   double jetR = 0.4;
-  hB->GetYaxis()->SetRangeUser(min_pt, max_pt);
-  TH1F *hBaryon = (TH1F*)hB->ProjectionX();
-  hM->GetYaxis()->SetRangeUser(min_pt, max_pt);
-  TH1F *hMeson = (TH1F*)hM->ProjectionX();
-  // hBaryon->Scale(1./hBaryon->Integral());
-  // hMeson->Scale(1./hMeson->Integral());
+  std::vector<TH1F*> hists;
+  std::vector<TH1F*> ratios;
+  TH1F *hRatio, *ptRatio;
 
-  // Top plot settings
-  hBaryon->SetStats(0);
-  hBaryon->SetTitle(TString::Format("%s/%s, pt_{ch} #in [%.0f,%.0f) GeV/c, R = %.1f %s",
-                                    baryon.c_str(), meson.c_str(), min_pt, max_pt, jetR, setting.c_str()).Data());
-  hBaryon->GetYaxis()->SetTitle(TString::Format("%s/%s", baryon.c_str(), meson.c_str()).Data());
-  hBaryon->GetYaxis()->SetTitleFont(43);
-  hBaryon->GetYaxis()->SetTitleSize(20);
-  hBaryon->GetYaxis()->SetTitleOffset(1.5);
-  hBaryon->GetYaxis()->SetLabelFont(43);
-  hBaryon->GetYaxis()->SetLabelSize(14);
+  TCanvas *c_obs = new TCanvas(TString::Format("c_%s_%s_%s", sNum.c_str(), sDen.c_str(), setting.c_str()).Data(),
+                               TString::Format("%s_%s_%s", sNum.c_str(), sDen.c_str(), setting.c_str()).Data(),
+                               1200, 800);
+  TPad *pad1 = new TPad("pad1", "pad1", 0, 0.35, 1, 0.9);
+  pad1->Draw();
+  c_obs->cd();
+  TPad *pad2 = new TPad("pad2", "pad2", 0, 0, 1, 0.4);
+  pad2->Draw();
+  pad1->cd();
+  auto legend = CreateLegend(0.25, 0.45, 0.6, 0.9, "", 0.05);
 
-  hMeson->SetStats(0);
-  hMeson->SetMarkerStyle(kMultiply);
-  hMeson->SetLineColor(kRed);
-  hMeson->SetMarkerColor(kRed);
+  int ptNum = ptBins.size()-1;
+  ptNum = 3;
+  for (int i = 0; i < ptNum; i++){
+    double min_pt = ptBins[i];
+    double max_pt = ptBins[i+1];
 
-  // Ratio plot settings
-  TH1F *ratio = (TH1F*)hBaryon->Clone("Ratio Base/Base");
-  ratio->Divide(hMeson);
-  // ratio->SetTitle("");
-  ratio->GetXaxis()->SetTitle(TString::Format("#it{p}_T").Data());
-  ratio->GetXaxis()->SetTitleFont(43);
-  ratio->GetXaxis()->SetTitleSize(20);
-  // ratio->GetXaxis()->SetTitleOffset(2.5);
-  ratio->GetXaxis()->SetLabelFont(43);
-  ratio->GetXaxis()->SetLabelSize(14);
-  ratio->GetYaxis()->SetTitle("Ratio");
-  ratio->GetYaxis()->SetTitleFont(43);
-  ratio->GetYaxis()->SetTitleSize(20);
-  // ratio->GetYaxis()->SetTitleOffset(1.5);
-  ratio->GetYaxis()->SetLabelFont(43);
-  ratio->GetYaxis()->SetLabelSize(14);
+    hRatio = make_ratio(hNum, sNum, hDen, sDen, min_pt, max_pt);
+    hRatio->SetMarkerStyle(GetMarker(i));
+    hRatio->SetMarkerColor(GetColor(i));
+    hRatio->SetLineColor(GetColor(i));
+    legend->AddEntry(hRatio, TString::Format("#it{p}_{T}^{ch jet} %.0f - %.0f", min_pt, max_pt).Data());
+    hists.push_back(hRatio);
 
-  TH1F *ratioBaryon = (TH1F*)hBaryon->Clone("Ratio X/Base");
-  ratioBaryon->Divide(hMeson);
+    ptRatio = (TH1F*)hRatio->Clone("bin/base");
+    ptRatio->Divide(hists[0]);
+    ratios.push_back(ptRatio);
+  }
 
-  int XmaxBinR = ratio->FindLastBinAbove(0,1);
-  // int XminBinR = ratio->FindFirstBinAbove(0,1);
-  // double Ymin = min({hBaryon->GetMinimum(), hMeson->GetMinimum()});
-  // Ymin *= 0.5;
-  // double Ymax = max({hBaryon->GetMaximum(), hMeson->GetMaximum()});
-  // Ymax *= 2;
-  // if (Ymin == 0){
-  //   double tmpBaryon = hBaryon->FindLastBinAbove(0,1);
-  //   double tmpMeson = hMeson->FindLastBinAbove(0,1);
-  //   Ymin = min({hBaryon->GetBinContent(tmpBaryon), hMeson->GetBinContent(tmpMeson)});
-  // }
-  double YminR = ratio->GetMinimum();
-  // YminR *= 0.9;
-  double YmaxR = ratio->GetMaximum();
-  if (YmaxR > 0.8) YmaxR = 0.8;
-  // else YmaxR *= 1.1;
+  string yTitle;
+  if (sDen == "pi" || sDen == "pi0"){
+    yTitle = TString::Format("%s/#%s (%s)", sNum.c_str(), sDen.c_str(), setting.c_str()).Data();
+  }
+  else{
+    yTitle = TString::Format("%s/%s (%s)", sNum.c_str(), sDen.c_str(), setting.c_str()).Data();
+  }
+  double xmin = 0, xmax = 0, ymin = 0, ymax = 0, yminr = 0, ymaxr = 0;
+  for (auto hist : hists){
+    int lastBin = hist->FindLastBinAbove(0,1);
+    xmax = max(xmax, hist->GetBinLowEdge(lastBin) + hist->GetBinWidth(lastBin));
+  }
+
+  /* If you want to manually set the range, do it here.
+  xmin = 5;
+  xmax = 15;
   // */
 
-  // hBaryon->GetXaxis()->SetRange(0,XmaxBinR);
-  ratio->GetXaxis()->SetRange(0,XmaxBinR);
+  for (auto hist : hists){
+    hist->GetXaxis()->SetRangeUser(xmin, xmax);
+    ymax = max(ymax, hist->GetMaximum());
+  }
+  for (auto ratio : ratios){
+    ratio->GetXaxis()->SetRangeUser(xmin, xmax);
+    ymaxr = max(ymax, ratio->GetMaximum());
+  }
+  if (ymax > 3) ymax = 3;
+  if (ymaxr > 3) ymaxr = 3;
 
-  // hBaryon->GetYaxis()->SetRangeUser(Ymin, Ymax);
-  ratio->GetYaxis()->SetRangeUser(YminR,YmaxR);
-
-  TCanvas *c_obs = new TCanvas(TString::Format("c_%s_pt_%.0f_%.0f", obs.c_str(), min_pt, max_pt).Data(),
-                               TString::Format("%s_pt_%.0f_%.0f", obs.c_str(), min_pt, max_pt).Data(),
-                               900, 600);
-  // TPad *pad1 = new TPad("pad1","pad1",0,0.4,1,1);
-  // pad1->SetBottomMargin(0);
-  // pad1->SetLeftMargin(0.15);
-  // pad1->Draw();
-  // // pad1->SetLogy();
-  // if (obs == "orth") pad1->SetLogx();
-  // pad1->cd();
-
-  // hBaryon->Draw();
-  // if (hMeson->GetEntries() != 0) hMeson->Draw("same");
-
-  auto legend = new TLegend(0.1,0.7,0.3,0.9); // Top right corner
-  if (legend) std::cout << "legend" << std::endl;
-  // if (obs == "orth"){
-  //   delete legend;
-  //   legend = new TLegend(0.4, 0, 0.7, 0.2); // Bottom middle
-  // }
-  // legend->AddEntry(hBaryon, baryon.c_str());
-  // legend->AddEntry(hMeson, meson.c_str());
-  legend->AddEntry(ratioBaryon, TString::Format("#it{p}_{T}^{ch jet} %.0f - %.0f", min_pt, max_pt).Data());
-
-
-  // c_obs->cd();
-  // TPad *pad2 = new TPad("pad2","pad2",0,0.1,1,0.4);
-  // pad2->SetTopMargin(0);
-  // pad2->SetBottomMargin(0.25);
-  // pad2->SetLeftMargin(0.15);
-  // pad2->Draw();
-  // if (obs == "orth") pad2->SetLogx();
-  // pad2->cd();
-
-  ratio->Draw();
+  TH1F* frame1 = DrawFrame(xmin, xmax, ymin, ymax, "", yTitle);
+  frame1->Draw();
+  for (auto hist : hists){
+    hist->Draw("same");
+  }
   legend->Draw("same");
-  // if (ratioBaryon->GetEntries() != 0) ratioBaryon->Draw();
-  // c_obs->cd();
 
-  // c_obs->SaveAs(TString::Format("../plots/fragmentation/ChargedVsFull/%s_%s_%s_ptch_%.0f_%.0f.pdf",
-                                // setting.c_str(), hadron.c_str(), obs.c_str(), min_pt, max_pt).Data());
+  pad2->cd();
+  TH1F* frame2 = DrawFrame(xmin, xmax, yminr, ymaxr, "#it{p}_{T}^{ had}", "Ratio");
+  frame2->Draw("same");
+  for (auto ratio : ratios){
+    ratio->Draw("same");
+  }
+  c_obs->cd();
+  c_obs->SaveAs(TString::Format("../plots/fragmentation/HadronRatios/%s_%s_%s_ratio_%.0f_%.0f.pdf",
+                                setting.c_str(), sNum.c_str(), sDen.c_str(), xmin, xmax).Data());
   // delete c_obs;
+}
+
+TH1F* make_ratio(TH2F* hNum, string sNum, TH2F* hDen, string sDen, double min_pt, double max_pt){
+  hNum->GetYaxis()->SetRangeUser(min_pt, max_pt);
+  TH1F *hNumerator = (TH1F*)hNum->ProjectionX();
+  hDen->GetYaxis()->SetRangeUser(min_pt, max_pt);
+  TH1F *hDenominator = (TH1F*)hDen->ProjectionX();
+
+  // Top plot settings
+  hNumerator->SetStats(0);
+  hNumerator->SetTitle(TString::Format("%s/%s, pt_{ch} #in [%.0f,%.0f) GeV/c",
+                                       sNum.c_str(), sDen.c_str(), min_pt, max_pt).Data());
+  hNumerator->GetYaxis()->SetTitle(TString::Format("%s/%s", sNum.c_str(), sDen.c_str()).Data());
+  hDenominator->SetStats(0);
+
+  // Ratio plot settings
+  TH1F *ratio = (TH1F*)hNumerator->Clone("Ratio baryon/meson");
+  ratio->Divide(hDenominator);
+  ratio->GetXaxis()->SetTitle(TString::Format("#it{p}_{T}").Data());
+  return ratio;
 }
 
 void fit_spectra(TH1F* hFull, TH1F* hCharged, string obs, string hadron){

09b188fc3ca325045cb74d97b9a8f6d7f771010a
GijsvWeelden <55794847+GijsvWeelden@users.noreply.github.com>
2023-07-06 13:54:06 +0200

    Added legendTitle support, templated loadHist, added plotNHists without TF1 args

--- plotting_macros/histUtils.C
+++ plotting_macros/histUtils.C
@@ -29,8 +29,15 @@ T loadHist(TFile* inFile, string histName)
   return hist;
 }
 
+template <typename T>
+T loadHist(TDirectory* inDir, string histName)
+{
+  T hist = (T)inDir->Get(TString::Format("%s", histName.c_str()).Data());
+  return hist;
+}
+
 // 1D histogram version
-void plotOneHist(TH1F* hist, string xTitle, string yTitle, string histTitle, string saveName,
+void plotOneHist(TH1F* hist, string xTitle, string yTitle, string histTitle, string legendTitle, string saveName,
                  double xMinFrame, double xMaxFrame, double yMinFrame, double yMaxFrame,
                  double xMinLegend, double xMaxLegend, double yMinLegend, double yMaxLegend,
                  bool setLogY, bool drawLegend)
@@ -42,10 +49,14 @@ void plotOneHist(TH1F* hist, string xTitle, string yTitle, string histTitle, str
   TH1F* frame = DrawFrame(xMinFrame, xMaxFrame, yMinFrame, yMaxFrame, xTitle, yTitle);
   frame->SetTitle(TString::Format("%s", histTitle.c_str()).Data());
   frame->Draw();
+  hist->SetLineColor(GetColor(0));
+  hist->SetMarkerColor(GetColor(0));
+  hist->SetMarkerStyle(GetMarker(0));
+  hist->SetLineWidth(3);
   hist->Draw("same, hist");
 
   if (drawLegend) {
-    auto legend = CreateLegend(xMinLegend, xMaxLegend, yMinLegend, yMaxLegend);
+    auto legend = CreateLegend(xMinLegend, xMaxLegend, yMinLegend, yMaxLegend, legendTitle);
     legend->SetTextFont(42);
     legend->SetTextSize(0.04);
     legend->SetBorderSize(0);
@@ -53,14 +64,14 @@ void plotOneHist(TH1F* hist, string xTitle, string yTitle, string histTitle, str
   }
 
   if (setLogY) { nc->SetLogy(); }
-  nc->SaveAs(TString::Format("Plots/%s.pdf", saveName.c_str()).Data());
+  nc->SaveAs(TString::Format("./%s.pdf", saveName.c_str()).Data());
   delete nc;
 }
 // 2D histogram version
 void plotOneHist(TH2F* hist, string xTitle, string yTitle, string histTitle, string saveName,
                  double xMinFrame, double xMaxFrame, double yMinFrame, double yMaxFrame,
                  double xMinLegend, double xMaxLegend, double yMinLegend, double yMaxLegend,
-                 bool setLogY, bool drawLegend)
+                 bool setLogZ, bool drawLegend)
 {
   auto nc = new TCanvas("Plot", "Plot", 600, 600);
   nc->SetLeftMargin(0.15);
@@ -68,6 +79,7 @@ void plotOneHist(TH2F* hist, string xTitle, string yTitle, string histTitle, str
 
   TH1F* frame = DrawFrame(xMinFrame, xMaxFrame, yMinFrame, yMaxFrame, xTitle, yTitle);
   frame->SetTitle(TString::Format("%s", histTitle.c_str()).Data());
+  frame->GetYaxis()->SetTitleOffset(1.3);
   frame->Draw();
   hist->Draw("same, colz");
 
@@ -79,10 +91,58 @@ void plotOneHist(TH2F* hist, string xTitle, string yTitle, string histTitle, str
     legend->AddEntry(hist, TString::Format("%s", histTitle.c_str()).Data());
   }
 
-  if (setLogY) { nc->SetLogy(); }
-  nc->SaveAs(TString::Format("Plots/%s.pdf", saveName.c_str()).Data());
+  if (setLogZ) { nc->SetLogz(); }
+  nc->SaveAs(TString::Format("./%s.pdf", saveName.c_str()).Data());
   delete nc;
 }
+// Plot only histograms
+void plotNHists(std::vector<TH1F*> histVector, std::vector<string> histNameVector,
+                string xTitle, string yTitle, string histTitle, string legendTitle, string saveName,
+                double xMinFrame, double xMaxFrame, double yMinFrame, double yMaxFrame,
+                double xMinLegend, double xMaxLegend, double yMinLegend, double yMaxLegend,
+                bool setLogY, string setHistDrawOption)
+{
+  // TODO: how to properly handle exceptions?
+  // Check if each histogram/function has an associated name
+  if (histVector.size() != histNameVector.size()){
+    cout << "plotNHists: hist vector and hist name vector have different sizes (" << histVector.size() << ", " << histNameVector.size() << ")" << endl << "Aborting" << endl;
+    return;
+  }
+
+  auto nc = new TCanvas("Plot", "Plot", 600, 600);
+  nc->SetLeftMargin(0.15);
+  nc->cd();
+
+  TH1F* frame = DrawFrame(xMinFrame, xMaxFrame, yMinFrame, yMaxFrame, xTitle, yTitle);
+  frame->SetTitle(TString::Format("%s", histTitle.c_str()).Data());
+  frame->Draw();
+  string histDrawOption = "same";
+  if (setHistDrawOption != "") {
+    histDrawOption = TString::Format("%s, %s", histDrawOption.c_str(), setHistDrawOption.c_str()).Data();
+  }
+
+  auto legend = CreateLegend(xMinLegend, xMaxLegend, yMinLegend, yMaxLegend, legendTitle);
+  legend->SetTextFont(42);
+  // legend->SetTextSize(0.04);
+  legend->SetTextSize(0.06);
+  legend->SetBorderSize(0);
+
+  for (unsigned int i = 0; i < histVector.size(); i++){
+    TH1F* hist = histVector[i];
+    string name = histNameVector[i];
+    hist->SetLineColor(GetColor(i));
+    hist->SetMarkerColor(GetColor(i));
+    hist->SetMarkerStyle(GetMarker(i));
+    hist->SetLineWidth(3);
+    legend->AddEntry(hist, TString::Format("%s", name.c_str()).Data());
+    hist->Draw(histDrawOption.c_str());
+  }
+  legend->Draw("same");
+  if (setLogY) { nc->SetLogy(); }
+  nc->SaveAs(TString::Format("./%s.pdf", saveName.c_str()).Data());
+  delete nc;
+}
+// Plot n histograms and n functions
 void plotNHists(std::vector<TH1F*> histVector, std::vector<string> histNameVector,
                 std::vector<TF1*> funcVector, std::vector<string> funcNameVector,
                 string xTitle, string yTitle, string histTitle, string saveName,
@@ -281,32 +341,32 @@ TH1F* projectHist(TH2F* inputHist, string projectionAxis, string histName, doubl
 //   return outputHist;
 // }
 // Project a 4D histogram onto a 2D histogram
-// TH2F* projectHist(THnF* inputHist, int projectionAxisX, int projectionAxisY,
-//                   double axis0Min, double axis0Max, double axis1Min, double axis1Max,
-//                   double axis2Min, double axis2Max, double axis3Min, double axis3Max)
-// {
-//   // TH2F* outputHist;
-//   THnF* inputClone = (THnF*)inputHist->Clone("CloneOfInput");
+TH2F* projectHist(THnF* inputHist, int projectionAxisX, int projectionAxisY,
+                  double axis0Min, double axis0Max, double axis1Min, double axis1Max,
+                  double axis2Min, double axis2Max, double axis3Min, double axis3Max)
+{
+  // TH2F* outputHist;
+  THnF* inputClone = (THnF*)inputHist->Clone("CloneOfInput");
 
-//   int firstBinAxis0 = 1; int lastBinAxis0 = inputClone->GetAxis(0)->GetNbins() + 1;
-//   int firstBinAxis1 = 1; int lastBinAxis1 = inputClone->GetAxis(1)->GetNbins() + 1;
-//   int firstBinAxis2 = 1; int lastBinAxis2 = inputClone->GetAxis(2)->GetNbins() + 1;
-//   int firstBinAxis3 = 1; int lastBinAxis3 = inputClone->GetAxis(3)->GetNbins() + 1;
+  int firstBinAxis0 = 1; int lastBinAxis0 = inputClone->GetAxis(0)->GetNbins() + 1;
+  int firstBinAxis1 = 1; int lastBinAxis1 = inputClone->GetAxis(1)->GetNbins() + 1;
+  int firstBinAxis2 = 1; int lastBinAxis2 = inputClone->GetAxis(2)->GetNbins() + 1;
+  int firstBinAxis3 = 1; int lastBinAxis3 = inputClone->GetAxis(3)->GetNbins() + 1;
 
-//   if (axis0Min > -900) { firstBinAxis0 = inputClone->GetAxis(0)->FindBin(axis0Min); }
-//   if (axis0Max > -900) { lastBinAxis0 = inputClone->GetAxis(0)->FindBin(axis0Max); }
-//   if (axis1Min > -900) { firstBinAxis1 = inputClone->GetAxis(1)->FindBin(axis1Min); }
-//   if (axis1Max > -900) { lastBinAxis1 = inputClone->GetAxis(1)->FindBin(axis1Max); }
-//   if (axis2Min > -900) { firstBinAxis2 = inputClone->GetAxis(2)->FindBin(axis2Min); }
-//   if (axis2Max > -900) { lastBinAxis2 = inputClone->GetAxis(2)->FindBin(axis2Max); }
-//   if (axis3Min > -900) { firstBinAxis3 = inputClone->GetAxis(3)->FindBin(axis3Min); }
-//   if (axis3Max > -900) { lastBinAxis3 = inputClone->GetAxis(3)->FindBin(axis3Max); }
+  if (axis0Min > -900) { firstBinAxis0 = inputClone->GetAxis(0)->FindBin(axis0Min); }
+  if (axis0Max > -900) { lastBinAxis0 = inputClone->GetAxis(0)->FindBin(axis0Max); }
+  if (axis1Min > -900) { firstBinAxis1 = inputClone->GetAxis(1)->FindBin(axis1Min); }
+  if (axis1Max > -900) { lastBinAxis1 = inputClone->GetAxis(1)->FindBin(axis1Max); }
+  if (axis2Min > -900) { firstBinAxis2 = inputClone->GetAxis(2)->FindBin(axis2Min); }
+  if (axis2Max > -900) { lastBinAxis2 = inputClone->GetAxis(2)->FindBin(axis2Max); }
+  if (axis3Min > -900) { firstBinAxis3 = inputClone->GetAxis(3)->FindBin(axis3Min); }
+  if (axis3Max > -900) { lastBinAxis3 = inputClone->GetAxis(3)->FindBin(axis3Max); }
 
-//   inputClone->GetAxis(0)->SetRange(firstBinAxis0, lastBinAxis0);
-//   inputClone->GetAxis(1)->SetRange(firstBinAxis1, lastBinAxis1);
-//   inputClone->GetAxis(2)->SetRange(firstBinAxis2, lastBinAxis2);
-//   inputClone->GetAxis(3)->SetRange(firstBinAxis3, lastBinAxis3);
-//   TH2F* outputHist = (TH2F*)inputClone->Projection(projectionAxisY, projectionAxisX);
-//   delete inputClone;
-//   return outputHist;
-// }
+  inputClone->GetAxis(0)->SetRange(firstBinAxis0, lastBinAxis0);
+  inputClone->GetAxis(1)->SetRange(firstBinAxis1, lastBinAxis1);
+  inputClone->GetAxis(2)->SetRange(firstBinAxis2, lastBinAxis2);
+  inputClone->GetAxis(3)->SetRange(firstBinAxis3, lastBinAxis3);
+  TH2F* outputHist = (TH2F*)inputClone->Projection(projectionAxisY, projectionAxisX);
+  delete inputClone;
+  return outputHist;
+}

9f14ff5b1cb83e6ae8243a172e72caff1aca5239
Gijs van Weelden <g.van.weelden@nikhef.nl>
2023-10-23 14:47:45 +0200

    Added Pt histograms

--- Unfolding/ClosureTest.C
+++ Unfolding/ClosureTest.C
@@ -211,82 +211,148 @@ void ClosureTest(string testFileName = "AnalysisResults.root", string responseFi
   //Create the Bayesian unfolding object
   RooUnfoldBayes Unfolding_bayes(ruResponse, testResponseMatrixDetectorProjection_rebinned, N_ITER, doSmoothing, unfoldName.c_str(), unfoldTitle.c_str());
   //Extracting the unfolded distribution as a histogram
-  TH2F* unfoldedTestZ = (TH2F*)Unfolding_bayes.Hreco(RooUnfold::kCovToy);
-  unfoldedTestZ->SetName("unfoldedTestZ");
+  TH2F* unfoldedTest = (TH2F*)Unfolding_bayes.Hreco(RooUnfold::kCovToy);
+  unfoldedTest->SetName("unfoldedTest");
   auto Unf_Bayes = &Unfolding_bayes;
   //Applying the Response matrix to the unfolded result
-  TH2F* refoldedTestZ = (TH2F*)ruResponse->ApplyToTruth(unfoldedTestZ, "refoldedTestZ");
+  TH2F* refoldedTest = (TH2F*)ruResponse->ApplyToTruth(unfoldedTest, "refoldedTest");
   //Include the fakes in order to compare with the input distribution
-  refoldedTestZ->Add(hDetectorFake);
+  refoldedTest->Add(hDetectorFake);
 
   // The unfolding is complete, the following sections refer to projections in one dimension and calculating the ratio between distributions
   Int_t ptBinLow      = hDetector->GetXaxis()->FindBin(PT_LOW);
   Int_t ptBinHigh     = hDetector->GetXaxis()->FindBin(PT_HIGH) - 1;
   Int_t ptBinLow_gen  = hTruth->GetXaxis()->FindBin(PT_LOW);
   Int_t ptBinHigh_gen = hTruth->GetXaxis()->FindBin(PT_HIGH) - 1;
-  string projectionName = TString::Format("d#it{N}/d#it{z}, #it{p}_{T}^{jet} #in [%d, %d] GeV/#it{c}", PT_LOW, PT_HIGH).Data();
-  string ratioName      = TString::Format("d#it{N}/d#it{z} ratio, #it{p}_{T}^{jet} #in [%d, %d] GeV/#it{c}", PT_LOW, PT_HIGH).Data();
+  string projectionNameZ  = TString::Format("d#it{N}/d#it{z}, #it{p}_{T}^{jet} #in [%d, %d] GeV/#it{c}", PT_LOW, PT_HIGH).Data();
+  string ratioNameZ       = TString::Format("d#it{N}/d#it{z} ratio, #it{p}_{T}^{jet} #in [%d, %d] GeV/#it{c}", PT_LOW, PT_HIGH).Data();
+  string projectionNamePt = TString::Format("d#it{N}/d#it{p}_{T}, #it{p}_{T}^{jet} #in [%d, %d] GeV/#it{c}", PT_LOW, PT_HIGH).Data();
+  string ratioNamePt      = TString::Format("d#it{N}/d#it{p}_{T} ratio, #it{p}_{T}^{jet} #in [%d, %d] GeV/#it{c}", PT_LOW, PT_HIGH).Data();
 
   //--------------Training sample (Sample creating the response)--------------------//
     //Truth level
-  TH1D* trainingTruth = hTruth->ProjectionY("trainingTruth", ptBinLow_gen, ptBinHigh_gen);
-  trainingTruth->SetTitle(TString::Format("%s (truth level, training sample); #it{z}", projectionName.c_str()).Data());
-  trainingTruth->Scale(1./trainingTruth->Integral(), "width");
+  TH1D* trainingPtTruth = hTruth->ProjectionX("trainingPtTruth", ptBinLow_gen, ptBinHigh_gen);
+  trainingPtTruth->SetTitle(TString::Format("%s (truth level, training sample); #it{p}_{T}", projectionNamePt.c_str()).Data());
+  trainingPtTruth->Scale(1./trainingPtTruth->Integral(), "width");
+
+  TH1D* trainingZTruth = hTruth->ProjectionY("trainingZTruth", ptBinLow_gen, ptBinHigh_gen);
+  trainingZTruth->SetTitle(TString::Format("%s (truth level, training sample); #it{z}", projectionNameZ.c_str()).Data());
+  trainingZTruth->Scale(1./trainingZTruth->Integral(), "width");
     //Detector level
-  TH1D*trainingDetector = hDetector->ProjectionY("trainingDetector", ptBinLow, ptBinHigh);
-  trainingDetector->SetTitle(TString::Format("%s (MC Detector level, training sample); #it{z}", projectionName.c_str()).Data());
-  trainingDetector->Scale(1./trainingDetector->Integral(), "width");
+  TH1D* trainingPtDetector = hDetector->ProjectionX("trainingPtDetector", ptBinLow, ptBinHigh);
+  trainingPtDetector->SetTitle(TString::Format("%s (MC Detector level, training sample); #it{p}_{T}", projectionNamePt.c_str()).Data());
+  trainingPtDetector->Scale(1./trainingPtDetector->Integral(), "width");
+
+  TH1D* trainingZDetector = hDetector->ProjectionY("trainingZDetector", ptBinLow, ptBinHigh);
+  trainingZDetector->SetTitle(TString::Format("%s (MC Detector level, training sample); #it{z}", projectionNameZ.c_str()).Data());
+  trainingZDetector->Scale(1./trainingZDetector->Integral(), "width");
 
   //--------------Test sample (MC sample treated as pseudodata)--------------------//
     //Truth level
-  TH1D* testTruth = testResponseMatrixTruthProjection_rebinned->ProjectionY("testTruth", ptBinLow_gen, ptBinHigh_gen);
-  testTruth->SetTitle(TString::Format("%s (truth level, test sample); #it{z}", projectionName.c_str()).Data());
-  testTruth->Scale(1./testTruth->Integral(), "width");
+  TH1D* testPtTruth = testResponseMatrixTruthProjection_rebinned->ProjectionX("testPtTruth", ptBinLow_gen, ptBinHigh_gen);
+  testPtTruth->SetTitle(TString::Format("%s (truth level, test sample); #it{p}_{T}", projectionNamePt.c_str()).Data());
+  testPtTruth->Scale(1./testPtTruth->Integral(), "width");
+
+  TH1D* testZTruth = testResponseMatrixTruthProjection_rebinned->ProjectionY("testZTruth", ptBinLow_gen, ptBinHigh_gen);
+  testZTruth->SetTitle(TString::Format("%s (truth level, test sample); #it{z}", projectionNameZ.c_str()).Data());
+  testZTruth->Scale(1./testZTruth->Integral(), "width");
     //Detector level
-  TH1D* testDetector = testResponseMatrixDetectorProjection_rebinned->ProjectionY("testDetector", ptBinLow, ptBinHigh);
-  testDetector->SetTitle(TString::Format("%s (detector level, test sample); #it{z}", projectionName.c_str()).Data());
-  testDetector->Scale(1./testDetector->Integral(), "width");
+  TH1D* testPtDetector = testResponseMatrixDetectorProjection_rebinned->ProjectionX("testPtDetector", ptBinLow, ptBinHigh);
+  testPtDetector->SetTitle(TString::Format("%s (detector level, test sample); #it{p}_{T}", projectionNamePt.c_str()).Data());
+  testPtDetector->Scale(1./testPtDetector->Integral(), "width");
+
+  TH1D* testZDetector = testResponseMatrixDetectorProjection_rebinned->ProjectionY("testZDetector", ptBinLow, ptBinHigh);
+  testZDetector->SetTitle(TString::Format("%s (detector level, test sample); #it{z}", projectionNameZ.c_str()).Data());
+  testZDetector->Scale(1./testZDetector->Integral(), "width");
     //Unfolded
-  TH1D* testUnfolded = unfoldedTestZ->ProjectionY("testUnfolded");
-  testUnfolded->SetTitle(TString::Format("%s (unfolded, test sample); #it{z}", projectionName.c_str()));
-  testUnfolded->Scale(1./testUnfolded->Integral(), "width");
+  TH1D* testPtUnfolded = unfoldedTest->ProjectionX("testPtUnfolded");
+  testPtUnfolded->SetTitle(TString::Format("%s (unfolded, test sample); #it{p}_{T}", projectionNamePt.c_str()));
+  testPtUnfolded->Scale(1./testPtUnfolded->Integral(), "width");
+
+  TH1D* testZUnfolded = unfoldedTest->ProjectionY("testZUnfolded");
+  testZUnfolded->SetTitle(TString::Format("%s (unfolded, test sample); #it{z}", projectionNameZ.c_str()));
+  testZUnfolded->Scale(1./testZUnfolded->Integral(), "width");
     //Refolded
-  TH1D* testRefolded = refoldedTestZ->ProjectionY("testRefolded");
-  testRefolded->SetTitle(TString::Format("%s (refolded, test sample); #it{z}", projectionName.c_str()));
-  testRefolded->Scale(1./testRefolded->Integral(), "width");
+  TH1D* testPtRefolded = refoldedTest->ProjectionX("testPtRefolded");
+  testPtRefolded->SetTitle(TString::Format("%s (refolded, test sample); #it{z}", projectionNameZ.c_str()));
+  testPtRefolded->Scale(1./testPtRefolded->Integral(), "width");
+
+  TH1D* testZRefolded = refoldedTest->ProjectionY("testZRefolded");
+  testZRefolded->SetTitle(TString::Format("%s (refolded, test sample); #it{z}", projectionNameZ.c_str()));
+  testZRefolded->Scale(1./testZRefolded->Integral(), "width");
 
   // Ratios of the projections
   // Training distributions - Detector response corrections
-  TH1D* trainingTruthOverDetector = (TH1D*)trainingTruth->Clone("trainingTruthOverDetector");
-  trainingTruthOverDetector->Reset();
-  for (int iBin = 0; iBin < trainingDetector->GetNbinsX(); iBin++) {
-    trainingTruthOverDetector->SetBinContent(iBin, trainingDetector->GetBinContent(iBin));
-    trainingTruthOverDetector->SetBinError(iBin, trainingDetector->GetBinError(iBin));
+  TH1D* trainingPtTruthOverDetector = (TH1D*)trainingPtTruth->Clone("trainingPtTruthOverDetector");
+  trainingPtTruthOverDetector->Reset();
+  for (int iBin = 0; iBin < trainingPtDetector->GetNbinsX(); iBin++) {
+    trainingPtTruthOverDetector->SetBinContent(iBin, trainingPtDetector->GetBinContent(iBin));
+    trainingPtTruthOverDetector->SetBinError(iBin, trainingPtDetector->GetBinError(iBin));
   }
-  trainingTruthOverDetector->Divide(trainingTruth, trainingTruthOverDetector);
-  trainingTruthOverDetector->SetTitle(TString::Format("Truth over Detector %s (training sample); #it{z}; #frac{Truth}{Detector}", ratioName.c_str()));
+  trainingPtTruthOverDetector->Print();
+  trainingPtTruthOverDetector->Divide(trainingPtTruth, trainingPtTruthOverDetector);
+  trainingPtTruthOverDetector->SetTitle(TString::Format("Truth over Detector %s (training sample); #it{p}_{T}; #frac{Truth}{Detector}", ratioNamePt.c_str()));
+
+  TH1D* trainingZTruthOverDetector = (TH1D*)trainingZTruth->Clone("trainingZTruthOverDetector");
+  trainingZTruthOverDetector->Reset();
+  for (int iBin = 0; iBin < trainingZDetector->GetNbinsX(); iBin++) {
+    trainingZTruthOverDetector->SetBinContent(iBin, trainingZDetector->GetBinContent(iBin));
+    trainingZTruthOverDetector->SetBinError(iBin, trainingZDetector->GetBinError(iBin));
+  }
+  trainingZTruthOverDetector->Print();
+  trainingZTruthOverDetector->Divide(trainingZTruth, trainingZTruthOverDetector);
+  trainingZTruthOverDetector->SetTitle(TString::Format("Truth over Detector %s (training sample); #it{z}; #frac{Truth}{Detector}", ratioNameZ.c_str()));
 
   //Test distributions - Detector response corrections
-  TH1D* testUnfoldedOverDetector = (TH1D*)testUnfolded->Clone("testUnfoldedOverDetector");
-  testUnfoldedOverDetector->Reset();
-  for (int iBin = 0; iBin < testDetector->GetNbinsX(); iBin++) {
-    testUnfoldedOverDetector->SetBinContent(iBin, testDetector->GetBinContent(iBin));
-    testUnfoldedOverDetector->SetBinError(iBin, testDetector->GetBinError(iBin));
+  TH1D* testPtUnfoldedOverDetector = (TH1D*)testPtUnfolded->Clone("testPtUnfoldedOverDetector");
+  testPtUnfoldedOverDetector->Reset();
+  for (int iBin = 0; iBin < testPtDetector->GetNbinsX(); iBin++) {
+    testPtUnfoldedOverDetector->SetBinContent(iBin, testPtDetector->GetBinContent(iBin));
+    testPtUnfoldedOverDetector->SetBinError(iBin, testPtDetector->GetBinError(iBin));
   }
-  testUnfoldedOverDetector->Divide(testUnfolded, testUnfoldedOverDetector);
-  testUnfoldedOverDetector->SetTitle(TString::Format("Unfolded over Detector %s (test sample); #it{z}; #frac{Unfolded}{Detector}", ratioName.c_str()));
+  testPtUnfoldedOverDetector->Print();
+  testPtUnfoldedOverDetector->Divide(testPtUnfolded, testPtUnfoldedOverDetector);
+  testPtUnfoldedOverDetector->SetTitle(TString::Format("Unfolded over Detector %s (test sample); #it{z}; #frac{Unfolded}{Detector}", ratioNamePt.c_str()));
+
+  TH1D* testZUnfoldedOverDetector = (TH1D*)testZUnfolded->Clone("testZUnfoldedOverDetector");
+  testZUnfoldedOverDetector->Reset();
+  for (int iBin = 0; iBin < testZDetector->GetNbinsX(); iBin++) {
+    testZUnfoldedOverDetector->SetBinContent(iBin, testZDetector->GetBinContent(iBin));
+    testZUnfoldedOverDetector->SetBinError(iBin, testZDetector->GetBinError(iBin));
+  }
+  testZUnfoldedOverDetector->Print();
+  testZUnfoldedOverDetector->Divide(testZUnfolded, testZUnfoldedOverDetector);
+  testZUnfoldedOverDetector->SetTitle(TString::Format("Unfolded over Detector %s (test sample); #it{z}; #frac{Unfolded}{Detector}", ratioNameZ.c_str()));
 
   //Closure on the truth level
-  TH1D* testUnfoldedOverTruth = (TH1D*)testUnfolded->Clone("testUnfoldedOverTruth");
-  testUnfoldedOverTruth->Reset();
-  testUnfoldedOverTruth->Divide(testUnfolded, testTruth);
-  testUnfoldedOverTruth->SetTitle(TString::Format("Unfolded over Truth %s (test sample); #it{z}; #frac{Unfolded}{Truth}", ratioName.c_str()));
+  TH1D* testPtUnfoldedOverTruth = (TH1D*)testPtUnfolded->Clone("testPtUnfoldedOverTruth");
+  testPtUnfoldedOverTruth->Reset();
+  // for (int iBin = 0; iBin < testPtTruth->GetNbinsX(); iBin++) {
+  //   testZUnfoldedOverDetector->SetBinContent(iBin, testPtTruth->GetBinContent(iBin));
+  //   testZUnfoldedOverDetector->SetBinError(iBin, testPtTruth->GetBinError(iBin));
+  // }
+  testPtUnfoldedOverTruth->Print();
+  testPtUnfoldedOverTruth->Divide(testPtUnfolded, testPtTruth);
+  testPtUnfoldedOverTruth->SetTitle(TString::Format("Unfolded over Truth %s (test sample); #it{p}_{T}; #frac{Unfolded}{Truth}", ratioNamePt.c_str()));
+
+  TH1D* testZUnfoldedOverTruth = (TH1D*)testZUnfolded->Clone("testZUnfoldedOverTruth");
+  testZUnfoldedOverTruth->Reset();
+  testZUnfoldedOverTruth->Print();
+  testZUnfoldedOverTruth->Divide(testZUnfolded, testZTruth);
+  testZUnfoldedOverTruth->SetTitle(TString::Format("Unfolded over Truth %s (test sample); #it{z}; #frac{Unfolded}{Truth}", ratioNameZ.c_str()));
 
   //Closure on the detector level
-  TH1D* testRefoldedOverDetector = (TH1D*)testRefolded->Clone("testRefoldedOverDetector");
-  testRefoldedOverDetector->Reset();
-  testRefoldedOverDetector->Divide(testRefolded, testDetector);
-  testRefoldedOverDetector->SetTitle(TString::Format("Refolded over Detector %s (test sample); #it{z}; #frac{Refolded}{Detector}",ratioName.c_str()));
+  TH1D* testPtRefoldedOverDetector = (TH1D*)testPtRefolded->Clone("testPtRefoldedOverDetector");
+  testPtRefoldedOverDetector->Reset();
+  testPtRefoldedOverDetector->Print();
+  testPtRefoldedOverDetector->Divide(testPtRefolded, testPtDetector);
+  testPtRefoldedOverDetector->SetTitle(TString::Format("Refolded over Detector %s (test sample); #it{p}_{T}; #frac{Refolded}{Detector}",ratioNamePt.c_str()));
+
+  TH1D* testZRefoldedOverDetector = (TH1D*)testZRefolded->Clone("testZRefoldedOverDetector");
+  testZRefoldedOverDetector->Reset();
+  testZRefoldedOverDetector->Print();
+  testZRefoldedOverDetector->Divide(testZRefolded, testZDetector);
+  testZRefoldedOverDetector->SetTitle(TString::Format("Refolded over Detector %s (test sample); #it{z}; #frac{Refolded}{Detector}",ratioNameZ.c_str()));
 
   //Saving the output in a new file
   string outFileName = TString::Format("closureTest_binWidth%d_pt%d-%d_ptmin%.0f_z%.0f-%.0f_nIter%d.root",
@@ -310,19 +376,29 @@ void ClosureTest(string testFileName = "AnalysisResults.root", string responseFi
   testResponseMatrixTruthProjection_rebinned->Write(testResponseMatrixTruthProjection_rebinned->GetName(), TObject::kOverwrite);
   testResponseMatrixDetectorProjection_rebinned->Write(testResponseMatrixDetectorProjection_rebinned->GetName(), TObject::kOverwrite);
   Unf_Bayes->Write(Unf_Bayes->GetName(), TObject::kOverwrite);
-  unfoldedTestZ->Write(unfoldedTestZ->GetName(), TObject::kOverwrite);
-  refoldedTestZ->Write(refoldedTestZ->GetName(), TObject::kOverwrite);
-  trainingDetector->Write(trainingDetector->GetName(), TObject::kOverwrite);
-  trainingTruth->Write(trainingTruth->GetName(), TObject::kOverwrite);
-  testDetector->Write(testDetector->GetName(), TObject::kOverwrite);
-  testTruth->Write(testTruth->GetName(), TObject::kOverwrite);
-  testUnfolded->Write(testUnfolded->GetName(), TObject::kOverwrite);
-  testRefolded->Write(testRefolded->GetName(), TObject::kOverwrite);
+  unfoldedTest->Write(unfoldedTest->GetName(), TObject::kOverwrite);
+  refoldedTest->Write(refoldedTest->GetName(), TObject::kOverwrite);
+  trainingPtDetector->Write(trainingPtDetector->GetName(), TObject::kOverwrite);
+  trainingZDetector->Write(trainingZDetector->GetName(), TObject::kOverwrite);
+  trainingPtTruth->Write(trainingPtTruth->GetName(), TObject::kOverwrite);
+  trainingZTruth->Write(trainingZTruth->GetName(), TObject::kOverwrite);
+  testPtDetector->Write(testPtDetector->GetName(), TObject::kOverwrite);
+  testZDetector->Write(testZDetector->GetName(), TObject::kOverwrite);
+  testPtTruth->Write(testPtTruth->GetName(), TObject::kOverwrite);
+  testZTruth->Write(testZTruth->GetName(), TObject::kOverwrite);
+  testPtUnfolded->Write(testPtUnfolded->GetName(), TObject::kOverwrite);
+  testZUnfolded->Write(testZUnfolded->GetName(), TObject::kOverwrite);
+  testPtRefolded->Write(testPtRefolded->GetName(), TObject::kOverwrite);
+  testZRefolded->Write(testZRefolded->GetName(), TObject::kOverwrite);
 
-  trainingTruthOverDetector->Write(trainingTruthOverDetector->GetName(), TObject::kOverwrite);
-  testUnfoldedOverDetector->Write(testUnfoldedOverDetector->GetName(), TObject::kOverwrite);
-  testUnfoldedOverTruth->Write(testUnfoldedOverTruth->GetName(), TObject::kOverwrite);
-  testRefoldedOverDetector->Write(testRefoldedOverDetector->GetName(), TObject::kOverwrite);
+  trainingPtTruthOverDetector->Write(trainingPtTruthOverDetector->GetName(), TObject::kOverwrite);
+  trainingZTruthOverDetector->Write(trainingZTruthOverDetector->GetName(), TObject::kOverwrite);
+  testPtUnfoldedOverDetector->Write(testPtUnfoldedOverDetector->GetName(), TObject::kOverwrite);
+  testZUnfoldedOverDetector->Write(testZUnfoldedOverDetector->GetName(), TObject::kOverwrite);
+  testPtUnfoldedOverTruth->Write(testPtUnfoldedOverTruth->GetName(), TObject::kOverwrite);
+  testZUnfoldedOverTruth->Write(testZUnfoldedOverTruth->GetName(), TObject::kOverwrite);
+  testPtRefoldedOverDetector->Write(testPtRefoldedOverDetector->GetName(), TObject::kOverwrite);
+  testZRefoldedOverDetector->Write(testZRefoldedOverDetector->GetName(), TObject::kOverwrite);
   testFile->Close();
   responseFile->Close();
   outFile->Close();

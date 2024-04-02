4313ccadbabfd3773cddfcd328f045ca612ec51c
Gijs van Weelden <55794847+GijsvWeelden@users.noreply.github.com>
2024-04-02 17:04:59 +0200

    Added jet energy scale plotter

--- plotting_macros/Matching/V0s/plotV0inJet.C
+++ plotting_macros/Matching/V0s/plotV0inJet.C
@@ -13,7 +13,7 @@
 #include "TString.h"
 #include "TLegend.h"
 
-#include "/Users/gijsvanweelden/Documents/Fragmentation/plotting_macros/histUtils.C"
+#include "../../histUtils.C"
 
 double getNjets(TFile* inFile, double jetptmin, double jetptmax)
 {
@@ -62,6 +62,65 @@ void matchedJetPt(string inName = "AnalysisResults.root")
   latex = CreateLatex(xLatex, yLatex, latexText, textSize);
 
   saveName = "matchedJetPt";
+  saveName = TString::Format("%s.pdf", saveName.c_str());
+  plotNHists(canvas, frame, histVector, legend, latex, saveName, "colz");
+}
+void jetEnergyScale(string inName = "AnalysisResults.root", double partjetptmin = 10., double partjetptmax = 200.)
+{
+  gStyle->SetNdivisions(505, "xy");
+
+  string saveName, histName, histTitle, xTitle, yTitle, legendTitle, latexText, dataSet;
+  double textSize = 0.04;
+  double labelSize = 0.04;
+  double titleSize = 0.04;
+
+  bool SetLogy = true;
+  double xMinFrame = -1., xMaxFrame = 10., yMinFrame = 1e-5, yMaxFrame = 2.;
+  double xMinLegend = 0.5, xMaxLegend = 0.9, yMinLegend = 0.6, yMaxLegend = 0.8;
+  double xLatex = 0.4, yLatex = 0.8;
+  int xCanvas = 900, yCanvas = 900;
+  int rebinNumber = 5;
+  xTitle = "(#it{p}_{T, jet}^{det.} - #it{p}_{T, jet}^{part.})/#it{p}_{T, jet}^{part.}";
+  // yTitle = "#it{p}_{T, jet}^{part.} (GeV/#it{c})";
+  yTitle = "normalised count";
+  dataSet = "LHC23k4b_pass1";
+
+  std::vector<TH1D*> histVector;
+  TCanvas* canvas = new TCanvas("Plot", "Plot", xCanvas, yCanvas);
+  if (SetLogy) { canvas->SetLogy(); }
+  TH1F* frame = DrawFrame(xMinFrame, xMaxFrame, yMinFrame, yMaxFrame, xTitle, yTitle);
+  TLegend* legend = CreateLegend(xMinLegend, xMaxLegend, yMinLegend, yMaxLegend, legendTitle, textSize);
+  TLatex* latex;
+
+  // histName = "jet-fragmentation/matching/jets/matchPartJetptRelDiffPt";
+  histName = "jet-fragmentation_id10235/matching/jets/matchPartJetPtRelDiffPt";
+  TFile *inFile = TFile::Open(TString::Format("./%s", inName.c_str()).Data());
+  TH2D* th2 = (TH2D*)inFile->Get(histName.c_str());
+
+  std::array<int, 2> partjetptbins = getProjectionBins(th2->GetXaxis(), partjetptmin, partjetptmax);
+  TH1D* jes = th2->ProjectionY("jetEnergyScale", partjetptbins[0], partjetptbins[1]);
+  jes->Scale(1./ jes->Integral());
+  setStyle(jes, 0);
+  histVector.push_back(jes);
+
+  // Get RMS = jet resolution for given pt range
+  double rms = 0.;
+  for (int i = 1; i < jes->GetNbinsX(); i++) {
+    double binContent = jes->GetBinContent(i);
+    double binCenter = jes->GetBinCenter(i);
+    // Hist is self-normalised, so no need to divide by integral
+    rms += binContent * binCenter * binCenter;
+  }
+  rms = TMath::Sqrt(rms);
+  jes->Print("all");
+
+  double lowjetpt = th2->GetXaxis()->GetBinLowEdge(partjetptbins[0]);
+  double highjetpt = th2->GetXaxis()->GetBinUpEdge(partjetptbins[1]);
+  latexText = TString::Format("#splitline{ %s }{#splitline{ #it{p}_{T, jet}^{part.} = %.0f - %.0f GeV/#it{c} }{ RMS: %.2f } }", dataSet.c_str(), lowjetpt, highjetpt, rms).Data();
+  latex = CreateLatex(xLatex, yLatex, latexText, textSize);
+
+  saveName = "jetEnergyScale";
+  saveName = TString::Format("%s_partjetpt%.0f-%.0f", saveName.c_str(), partjetptmin, partjetptmax);
   saveName = TString::Format("%s.pdf", saveName.c_str());
   plotNHists(canvas, frame, histVector, legend, latex, saveName, "colz");
 }
